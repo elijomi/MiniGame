e8a2a504947fc7eadb8749516b645e96fea549b8
Elin <elin@mikira.se>
2025-08-29 21:58:35 +0200

    Solitaire: refactor card rendering to use SolitaireCard component and add code-behind for improved structure

---
+++ BrowserFun/Pages/Solitaire.razor.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+
+namespace BrowserFun.Pages;
+
+public partial class Solitaire : ComponentBase
+{
+    // Code-behind scaffold; logic remains in .razor for now.
+}
+
